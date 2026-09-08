a5b7dc5a6fcf60e5a3c5f5707288ed0619af4f23
daniel <6690680@gmail.com>
2026-09-08 12:08:59 +0300

    added consts from soldier.py to consts.py

--- soldier.py
+++ soldier.py
@@ -3,10 +3,6 @@ import pygame
 import consts
 import game_field
 
-STEP = 1
-X_COORDINATES = 0
-Y_COORDINATES = 1
-
 soldier = {}
 
 """ 
@@ -29,16 +25,16 @@ def get_new_coordinates(event_type):
     new_coordinates = ()
 
     if event_type == pygame.K_UP: # when player presses up key
-        new_coordinates = soldier["x"], soldier["y"] + STEP
+        new_coordinates = soldier["x"], soldier["y"] + consts.STEP
 
     elif event_type == pygame.K_DOWN: # when player presses down key
-        new_coordinates = soldier["x"], soldier["y"] - STEP
+        new_coordinates = soldier["x"], soldier["y"] - consts.STEP
 
     elif event_type == pygame.K_RIGHT: # when player presses right key
-        new_coordinates = soldier["x"] + STEP, soldier["y"]
+        new_coordinates = soldier["x"] + consts.STEP, soldier["y"]
 
     elif event_type == pygame.K_LEFT: # when player presses left key
-        new_coordinates = soldier["x"] - STEP, soldier["y"]
+        new_coordinates = soldier["x"] - consts.STEP, soldier["y"]
 
     if is_move_valid(new_coordinates):
         return new_coordinates
@@ -51,9 +47,9 @@ def get_new_coordinates(event_type):
     returns False, else the function returns True.
 """
 def is_move_valid(coordinates):
-    if coordinates[X_COORDINATES] < 0 or coordinates[X_COORDINATES] >= consts.BOARD_COLS:
+    if coordinates[consts.X_COORDINATES_INDEX] < 0 or coordinates[consts.X_COORDINATES_INDEX] >= consts.BOARD_COLS:
         return False
-    if coordinates[Y_COORDINATES] < 0 or coordinates[Y_COORDINATES] >= consts.BOARD_ROWS:
+    if coordinates[consts.Y_COORDINATES_INDEX] < 0 or coordinates[consts.Y_COORDINATES_INDEX] >= consts.BOARD_ROWS:
         return False
 
     return True
@@ -63,8 +59,8 @@ def is_move_valid(coordinates):
     and it updates the soldiers coordinates to the new coordinates.
 """
 def move_player(coordinates):
-    soldier["x"] = coordinates[X_COORDINATES]
-    soldier["y"] = coordinates[Y_COORDINATES]
+    soldier["x"] = coordinates[consts.X_COORDINATES_INDEX]
+    soldier["y"] = coordinates[consts.Y_COORDINATES_INDEX]
 
 """
     This function recieves the coordinates of the player and wants to check if there
@@ -74,7 +70,7 @@ def player_touch_mine(coordinates):
     for y in range(len(game_field.game_field)): # reversed y and x because of how 2d lists work
         for x in range(y):
             # if the coordinates of the soldier are the same as any mine on the map
-            if game_field.game_field[y][x] == consts.MINE_ING and coordinates[X_COORDINATES] == x and coordinates[Y_COORDINATES] == y:
+            if game_field.game_field[y][x] == consts.MINE_IMG and coordinates[consts.X_COORDINATES_INDEX] == x and coordinates[consts.Y_COORDINATES_INDEX] == y:
                 return True
     return False
 
@@ -84,6 +80,6 @@ def player_touch_mine(coordinates):
 """
 def player_touch_flag(coordinates):
     # if the coordinates of the soldier are the same as the flag
-    if coordinates[X_COORDINATES] == consts.BOARD_COLS - 1 and coordinates[Y_COORDINATES] == consts.BOARD_ROWS - 1:
+    if coordinates[consts.X_COORDINATES_INDEX] == consts.BOARD_COLS - 1 and coordinates[consts.Y_COORDINATES_INDEX] == consts.BOARD_ROWS - 1:
         return True
     return False
