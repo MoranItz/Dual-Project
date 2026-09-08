4028945b8fb9ef230bb907e8f0adb15e72b84f6b
daniel <6690680@gmail.com>
2026-09-08 12:03:47 +0300

    finished implementing the soldier.py file functions and added a few new draw functions in main to be declared in screen.py

--- game_field.py
+++ game_field.py
@@ -2,6 +2,8 @@ import pygame
 
 import soldier
 
+game_field = []
+
 def init_game_field():
     # TODO: initiate global game field object with randomly places mines and the player and flag inside
     pass
--- soldier.py
+++ soldier.py
@@ -1,26 +1,89 @@
+import pygame
 
+import consts
+import game_field
+
+STEP = 1
+X_COORDINATES = 0
+Y_COORDINATES = 1
+
+soldier = {}
+
+""" 
+    This function initiates the global soldier (player) object
+    it returns the object initiated with the [x, y] coordinates as [0, 0]
+    and his status (image) as the default soldier image
+"""
 def init_soldier():
-    global soldier
+    global soldier # creates global soldier object
     soldier = {"x" : 0, "y" : 0, "status" : ""}
 
     return soldier
 
+"""
+    This function gets the click the user pressed and returns the new
+    soldier coordinates based on the click. (up, down, right, left)
+    if a click is not one of those, the function just returns the current coordinates
+"""
 def get_new_coordinates(event_type):
-    #TODO: return new soldier move coordinates
-    pass
+    new_coordinates = ()
 
-def move_player(coordinates):
-    #TODO: move the player in the game field
-    pass
+    if event_type == pygame.K_UP: # when player presses up key
+        new_coordinates = soldier["x"], soldier["y"] + STEP
 
+    elif event_type == pygame.K_DOWN: # when player presses down key
+        new_coordinates = soldier["x"], soldier["y"] - STEP
+
+    elif event_type == pygame.K_RIGHT: # when player presses right key
+        new_coordinates = soldier["x"] + STEP, soldier["y"]
+
+    elif event_type == pygame.K_LEFT: # when player presses left key
+        new_coordinates = soldier["x"] - STEP, soldier["y"]
+
+    if is_move_valid(new_coordinates):
+        return new_coordinates
+
+    return soldier["x"], soldier["y"]
+
+"""
+    This function gets the new supposed coordinates for the new soldier move.
+    if the new coordinates exceed the boundries of the game_field the function
+    returns False, else the function returns True.
+"""
 def is_move_valid(coordinates):
-    #TODO: check if the move exits the boundries of the game field
-    pass
+    if coordinates[X_COORDINATES] < 0 or coordinates[X_COORDINATES] >= consts.BOARD_COLS:
+        return False
+    if coordinates[Y_COORDINATES] < 0 or coordinates[Y_COORDINATES] >= consts.BOARD_ROWS:
+        return False
 
+    return True
+
+"""
+    This function gets the new coordinates after the checks if the move is valid
+    and it updates the soldiers coordinates to the new coordinates.
+"""
+def move_player(coordinates):
+    soldier["x"] = coordinates[X_COORDINATES]
+    soldier["y"] = coordinates[Y_COORDINATES]
+
+"""
+    This function recieves the coordinates of the player and wants to check if there
+    is any collision with the player and any mine on the map.
+"""
 def player_touch_mine(coordinates):
-    #TODO: return true if player interacts with mine in game field
-    pass
+    for y in range(len(game_field.game_field)): # reversed y and x because of how 2d lists work
+        for x in range(y):
+            # if the coordinates of the soldier are the same as any mine on the map
+            if game_field.game_field[y][x] == consts.MINE_ING and coordinates[X_COORDINATES] == x and coordinates[Y_COORDINATES] == y:
+                return True
+    return False
 
+"""
+    This function recieves the coordinates of the player and wants to check if there
+    is any collision with the player and the flag. It returns True if it does and False if not
+"""
 def player_touch_flag(coordinates):
-    #TODO: return true if player interacts with flag in game field
-    pass
+    # if the coordinates of the soldier are the same as the flag
+    if coordinates[X_COORDINATES] == consts.BOARD_COLS - 1 and coordinates[Y_COORDINATES] == consts.BOARD_ROWS - 1:
+        return True
+    return False
