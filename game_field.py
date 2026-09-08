4b7b46c06a6b31d6858f3326141e500042dce901
Ponchik <prokopenkopolinka@gmail.com>
2026-09-08 11:31:33 +0300

    Moving draw functions to screen module

--- game_field.py
+++ game_field.py
@@ -5,19 +5,3 @@ import soldier
 def init_game_field():
     # TODO: initiate global game field object with randomly places mines and the player and flag inside
     pass
-
-def draw_field():
-    # TODO: draw current field
-    pass
-
-def draw_night_mode():
-    #TODO: draw the mines for 1 second for the player then reset back to normal
-    pass
-
-def draw_mine_explosion():
-    #TODO: draw mine explosion on screen
-    pass
-
-def draw_mine_hole():
-    #TODO: draw mine hole on screen
-    pass
