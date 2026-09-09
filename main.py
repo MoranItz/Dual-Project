a52bfbf8e430734c3ba884a530aca82ff7c306df
daniel <6690680@gmail.com>
2026-09-09 09:33:10 +0300

    push revert

--- main.py
+++ main.py
@@ -10,32 +10,30 @@ import time
 def main():
     game_over = False
 
+    soldier.init_soldier()
     game_field.init_game_field()
     screen.init_screen()
 
-    game_field.draw_field()
     while not game_over:
-
-        events = pygame.event.get() # get player events
-        for event in events:
-            if event.type == pygame.K_KP_ENTER: # if the player presses ENTER
-                game_field.show_mines(event.type)
-                time.sleep(1) # sleep for 1 second so the player will have a cooldown when seeing the mines
+        screen.draw_field()
+        for event in pygame.event.get():
+            if event.key == pygame.K_ESCAPE:
+                return
+            elif event.key == pygame.K_0:
+                screen.draw_night_mode()
+                # TIME SLEEP
             else:
-                soldier.move_player(event.type) # Moves player
+                soldier.move_player()
 
-        if soldier.player_touch_mine() or soldier.player_touch_flag():
-            game_over = True
+            if soldier.player_touch_flag():
+                print("win")
+                game_over = True
 
-        game_field.draw_field()
+            elif soldier.player_touch_mine():
+                print("lose")
+                game_over = True
 
 
-    if is_touch_flag():
-        print("game won!")
-    else:
-        print("game lost!")
-
-screen.close_screen()
 
 
 
